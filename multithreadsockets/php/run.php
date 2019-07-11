56f986e4eaec8149c9c6e2eb816da424849135fd
stfrha <mail@fredrikhoffman.se>
2019-07-12 00:22:45 +0200

    Updated homepage.

--- multithreadsockets/php/run.php
+++ multithreadsockets/php/run.php
@@ -5,25 +5,57 @@ error_reporting(E_ALL);
 include './socket.php';
 
 $RaspberryPiIP = "127.0.0.1"; // Change by your RaspberryPi/PC IP 
-$RaspberryPiPORT = 51717; // Change by your RaspberryPi / PC Port Number ..
+
+
+// Read port number from file: /home/pi/multithreadsockets/socket_config.txt
+$myfile = fopen("/home/pi/multithreadsockets/socket_config.txt", "r") or die("Unable to open file!");
+$portString = fgets($myfile);
+fclose($myfile);
+
+
+$RaspberryPiPORT = (int)$portString; // Change by your RaspberryPi / PC Port Number ..
 $connection = new Socket($RaspberryPiIP,$RaspberryPiPORT); // Create a new socet Connection object. 
 $connection->init();
 
 ////////  Get Command from front end page
 
-$on = "on";
-$off = "off";
+$autoMsg = "AUTO";
+$manualMsg = "MANL";
+$sonMsg = "S_ON";
+$soffMsg = "SOFF";
+$fonMsg = "F_ON";
+$foffMsg = "FOFF";
 $data = "";
 $operation = $_GET["op"];
-if ($operation =="on")
-{
 
-$command = $on;
+if ($operation =="auto")
+{
+   $command = $autoMsg;
 }
-if ($operation =="off")
-{
 
-$command = $off;
+if ($operation =="manl")
+{
+   $command = $manualMsg;
+}
+
+if ($operation =="s_on")
+{
+   $command = $sonMsg;
+}
+
+if ($operation =="soff")
+{
+   $command = $soffMsg;
+}
+
+if ($operation =="f_on")
+{
+   $command = $fonMsg;
+}
+
+if ($operation =="foff")
+{
+   $command = $foffMsg;
 }
 
 //// end Get Command from front end page
