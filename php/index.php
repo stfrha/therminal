351ba3b487fb1030e10e7339c9eea005b4399a29
Fredrik Hoffman <mail@fredrikhoffman.se>
2019-07-18 08:40:49 +0200

    Added leaner homepage.

--- php/index.php
+++ php/index.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set("display_errors",1);
 error_reporting(E_ALL);
 
@@ -12,7 +13,7 @@ $portString = fgets($myfile);
 fclose($myfile);
 
 $RaspberryPiPORT = (int)$portString; // Change by your RaspberryPi / PC Port Number ..
-$connection = new Socket($RaspberryPiIP,$RaspberryPiPORT); // Create a new socet Connection object. 
+$connection = new Socket($RaspberryPiIP,$RaspberryPiPORT); // Create a new socket object. 
 $connection->init();
 
 $connection->open_socket(); // Connect PHP to RaspberryPi or computer.
@@ -56,26 +57,41 @@ if (isset($_GET["op"]))
 	{
 	   $command = $foffMsg;
 	}
-	
-	$connection->send_data($command); //Send command String
-	
-	// We want the command to take effect before we query the 
-	// status. Hence a short sleep here
-	usleep(2000000);
-	
+   else
+   {
+      // In this case someone requested a page with an illegal
+      // command. Just answer with the status
+      $command = "SREQ";
+   }
 }
-
-$command = "SREQ";
-
+else
+{
+   // If no command is specified we query and display the status
+   $command = "SREQ";
+}
+	
 $connection->send_data($command); //Send command String
+
+
+// TODO: Wait for a response here. A response (like the one answering an SREQ
+// command) will always be sent, when the change has taken affect.
+
+// We want the command to take effect before we query the 
+// status. Hence a short sleep here
+// usleep(2000000);
+
+
+// Below is replaced with if-statement above
+// $command = "SREQ";
+// $connection->send_data($command); //Send command String
 
 $therminalStatus = $connection->read_data();
 
 // Latest status is on the form:
-// {pool temp: 06.5},{solar temp 33.9},{filter pump on/off},{solar pump on/off},{manual/auto}
-// Example: "06.2,33.9,on,off,auto"
+// {pool temp: 06.5};{solar temp 33.9};{filter pump on/off};{solar pump on/off};{manual/auto};{date};{time}
+// Example: "06.2;33.9;on;off;auto;2019-07-14;23:37:45"
 
-$statusArray = explode(",", $therminalStatus);
+$statusArray = explode(";", $therminalStatus);
 $poolTemp = $statusArray[0];
 $solarTemp  = $statusArray[1];
 $filterPump = $statusArray[2];
@@ -85,63 +101,64 @@ $state = $statusArray[4];
 $connection->close_socket(); 
 
 ?>
+
 <html>
    <head>
       <link rel="stylesheet" type="text/css" href="therminal.css" >
    </head>
    <body background="poolwater.jpg">
-<div class="poolTempSplit enabled selected">
-  <div class="centered">
-    <h2>Pool Temp: <?php echo $poolTemp; ?></h2>
-  </div>
-</div>
-
-<div class="solarTempSplit enabled selected">
-  <div class="centered">
-    <h2>Solar Temp: <?php echo $solarTemp; ?></h2>
-  </div>
-</div>
-
-<a href="?op=auto">  
-   <div class="topButtonSplit left enabled <?php if ($state == "manual") echo("notSelected"); else echo("selected"); ?>">
-     <div class="centered">
-       <h2>Auto</h2>
-     </div>
-   </div>
-</a>
-
-<a href="?op=manl"> 
-   <div class="topButtonSplit right enabled <?php if ($state == "auto") echo("notSelected"); else echo("selected"); ?>">
-     <div class="centered">
-       <h2>Manual</h2>
-     </div>
-   </div>
-</a>
-
-<a href="?op=<?php if ($filterPump == "1") echo "foff"; else echo "f_on";?>"> 
-   <div class="bottomButtonSplit left <?php if ($state == "manual") echo("enabled"); else echo("disabled");?> <?php if ($filterPump == "0") echo("notSelected"); else echo("selected"); ?>">
-     <div class="centered">
-       <h2>Filter Pump: <?php echo $filterPump; ?></h2>
-     </div>
-   </div>
-</a>
-
-<a href="?op=<?php if ($solarPump == "1") echo "soff"; else echo "s_on";?>"> 
-   <div class="bottomButtonSplit right <?php if ($state == "manual") echo("enabled"); else echo("disabled");?> <?php if ($solarPump == "0") echo("notSelected"); else echo("selected"); ?>">
-     <div class="centered">
-       <h2>Solar Pump: <?php echo $solarPump; ?></h2>
-     </div>
-   </div>
-</a>
-
-<a href="index.php"> 
-   <div class="refreshSplit enabled selected">
-      <div class="centered">
-         <h2>Refresh</h2>
+      <div class="poolTempSplit enabled notSelected">
+        <div class="centered">
+          <h2>Pool Temp: <?php echo $poolTemp; ?></h2>
+        </div>
       </div>
-   </div>
-</a> 
-</body>
+
+      <div class="solarTempSplit enabled notSelected">
+        <div class="centered">
+          <h2>Solar Temp: <?php echo $solarTemp; ?></h2>
+        </div>
+      </div>
+
+      <a href="?op=auto">  
+         <div class="topButtonSplit left enabled <?php if ($state == "manual") echo("notSelected"); else echo("selected"); ?>">
+           <div class="centered">
+             <h2>Auto</h2>
+           </div>
+         </div>
+      </a>
+
+      <a href="?op=manl"> 
+         <div class="topButtonSplit right enabled <?php if ($state == "auto") echo("notSelected"); else echo("selected"); ?>">
+           <div class="centered">
+             <h2>Manual</h2>
+           </div>
+         </div>
+      </a>
+
+      <a href="?op=<?php if ($filterPump == "1") echo "foff"; else echo "f_on";?>"> 
+         <div class="bottomButtonSplit left <?php if ($state == "manual") echo("enabled"); else echo("disabled");?> <?php if ($filterPump == "0") echo("notSelected"); else echo("selected"); ?>">
+           <div class="centered">
+             <h2>Filter Pump: <?php echo $filterPump; ?></h2>
+           </div>
+         </div>
+      </a>
+
+      <a href="?op=<?php if ($solarPump == "1") echo "soff"; else echo "s_on";?>"> 
+         <div class="bottomButtonSplit right <?php if ($state == "manual") echo("enabled"); else echo("disabled");?> <?php if ($solarPump == "0") echo("notSelected"); else echo("selected"); ?>">
+           <div class="centered">
+             <h2>Solar Pump: <?php echo $solarPump; ?></h2>
+           </div>
+         </div>
+      </a>
+
+      <a href="index.php"> 
+         <div class="refreshSplit enabled notSelected">
+            <div class="centered">
+               <h2>Refresh</h2>
+            </div>
+         </div>
+      </a> 
+   </body>
 </html>
 
 
